c56a61275c3b7b815422bae8984e4e480e62736b
Brianna Townsend <btownsend@eclipsemining.com>
2024-07-10 10:26:39 -0700

    fixed erroneous gettext newline append

--- TextEditor.cpp
+++ TextEditor.cpp
@@ -104,8 +104,9 @@ std::string TextEditor::GetText(const Coordinates & aStart, const Coordinates & 
 		else
 		{
 			istart = 0;
-			++lstart;
-			result += '\n';
+			if (++lstart < (int)mLines.size()) {
+				result += '\n';
+			}			
 		}
 	}
 
